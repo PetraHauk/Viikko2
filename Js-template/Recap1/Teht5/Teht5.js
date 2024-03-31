adfe9280be946911311e590c7ace3da91ddfc063
Petra Haukkala <petra.haukkala@gmail.com>
2024-03-31 23:15:59 +0300

    Tehtävät Commit

--- Js-template/Recap1/Teht5/Teht5.js
+++
@@ -1,8 +0,0 @@
-const number = +prompt('Anna positiivinen numero:');
-let sum = 0;
-
-for (let i = 0; i <= number; i++) {
-  sum += i;
-}
-
-document.querySelector('#target').innerHTML = 'Summa: ' + sum;
